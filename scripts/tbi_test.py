617948151e21a384a1414f3d05198213dd9af3e9
Tom Close <tclose@oist.jp>
2016-07-25 14:59:32 +1000

    debugging and added exceptions

---
+++ scripts/tbi_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import os.path
+import shutil
+from neuroanalysis import Scan, DiffusionDataset, LocalArchive
+from neuroanalysis.file_formats import mrtrix_format
+
+ARCHIVE_PATH = os.path.abspath(os.path.join(
+    os.environ['HOME'], 'Data', 'MBI', 'tbi', ))
+BASE_WORK_PATH = os.path.abspath(os.path.join(
+    os.environ['HOME'], 'Data', 'MBI', 'work'))
+
+WORK_PATH = os.path.abspath(os.path.join(BASE_WORK_PATH, 'tbi_test'))
+DATASET_NAME = 'tbi_test'
+
+shutil.rmtree(WORK_PATH, ignore_errors=True)
+os.makedirs(WORK_PATH)
+dataset = DiffusionDataset(
+    name=DATASET_NAME,
+    project_id='2_vs_2.5',
+    archive=LocalArchive(ARCHIVE_PATH),
+    input_scans={
+        'dwi_scan': Scan('R-L_60dir_b2000', mrtrix_format),
+        'forward_rpe': Scan('R-L_6dir_b0', mrtrix_format),
+        'reverse_rpe': Scan('L-R_6dir_b0', mrtrix_format)})
+dataset.fod_pipeline().run(work_dir=WORK_PATH)
