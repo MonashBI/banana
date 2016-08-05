15532f2fc4e6b2801c6b6ee78d11a5e91c94d781
Tom Close <tclose@oist.jp>
2016-08-05 17:23:28 +1000

    got LocalArchive sink and source to work with summary (i.e. per-subject and per-project) scans

--- nianalysis/interfaces/utils.py
+++ nianalysis/interfaces/utils.py
@@ -1,4 +1,4 @@
-from nipype.interfaces.base import TraitedSpec, traits
+from nipype.interfaces.base import TraitedSpec, traits, BaseInterface
 from nipype.interfaces.utility import Merge, MergeInputSpec
 
 
@@ -29,3 +29,34 @@ class MergeTuple(Merge):
         outputs = super(MergeTuple, self)._list_outputs()
         outputs['out'] = tuple(outputs['out'])
         return outputs
+
+
+class SplitSessionInputSpec(TraitedSpec):
+
+    session = traits.Tuple(
+        traits.Str(mandatory=True, desc="The subject ID"),
+        traits.Str(1, mandatory=True, usedefult=True,
+                   desc="The session or processed group ID"),)
+
+
+class SplitSessionOutpuSpec(TraitedSpec):
+
+    subject = traits.Str(mandatory=True, desc="The subject ID")
+
+    study = traits.Str(1, mandatory=True, usedefult=True,
+                       desc="The session or processed group ID")
+
+
+class SplitSession(BaseInterface):
+
+    input_spec = SplitSessionInputSpec
+    output_spec = SplitSessionOutpuSpec
+
+    def _list_outputs(self):
+        outputs = self._outputs().get()
+        outputs['subject'] = self.inputs.session[0]
+        outputs['study'] = self.inputs.session[1]
+        return outputs
+
+    def _run_interface(self, runtime):
+        return runtime
