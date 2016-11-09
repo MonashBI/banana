7ea8960820760c31348f09fddfaaf13207d53f02
Tom Close <tclose@oist.jp>
2016-11-09 17:19:28 +1100

    renamed file to dataset

--- scripts/aspree.py
+++ scripts/aspree.py
@@ -13,10 +13,10 @@ parser.add_argument('--subjects', type=str, nargs='+', default=None,
                     help=("Subject ids over which to run the diffusion "
                           "processing"))
 parser.add_argument('output_dir', type=str,
-                    help=("Output directory where FA, CSD and tracks files"
+                    help=("Output directory where FA, CSD and tracks datasets"
                           " are stored."))
 parser.add_argument('--working_dir', type=str, default=None,
-                    help=("The directory where the intermediate files are "
+                    help=("The directory where the intermediate datasets are "
                           "stored"))
 args = parser.parse_args()
 
