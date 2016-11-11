a56b92211cec72639043c2a52f46db772d5a8e62
Thomase G. Close <tom.g.close@gmail.com>
2016-11-11 22:00:37 +1000

    trying to get freesurfer aspree to work

--- scripts/aspree_freesurfer.py
+++ scripts/aspree_freesurfer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import os.path
 import shutil
-from nianalysis import Dataset, ASPREE_FSProject, LocalArchive
-from nianalysis.formats import dicom_format
+from nianalysis import Dataset
+from nianalysis.project.mri.structural import T1Project
+from nianalysis.archive.local import LocalArchive
+from nianalysis.data_formats import dicom_format
 
 ARCHIVE_PATH = os.path.abspath(os.path.join(
     os.environ['HOME'], 'Data', 'MBI', 'ARCHIVE_ROOT'))
@@ -15,9 +17,10 @@ WORK_PATH = os.path.abspath(os.path.join(BASE_WORK_PATH, DATASET_NAME))
 
 shutil.rmtree(WORK_PATH, ignore_errors=True)
 os.makedirs(WORK_PATH)
-project = NODDIProject(
+project = T1Project(
     name=ASPREE_FS_PROJECT,
     project_id=ASPREE_FS_PROJECT, archive=LocalArchive(ARCHIVE_PATH),
     input_datasets={
         't1': Dataset('13_t1_mprage_sag_p2_iso_1_ADNI', dicom_format)})
 project.freesurfer_pipeline().run(work_dir=WORK_PATH)
+
