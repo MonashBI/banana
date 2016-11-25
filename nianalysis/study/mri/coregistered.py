fbac4a66cc6a52d40066e3c660c3ba50837fc0c4
Tom Close <tclose@oist.jp>
2016-11-25 14:11:16 +1100

    coregistration pipelines pass unittest

--- nianalysis/study/mri/coregistered.py
+++ nianalysis/study/mri/coregistered.py
@@ -3,88 +3,109 @@ from nipype.interfaces.fsl import FLIRT
 from nianalysis.requirements import fsl5_req
 from nianalysis.citations import fsl_cite
 from nianalysis.data_formats import (
-    nifti_gz_format)
+    nifti_gz_format, text_matrix_format)
 from ..base import set_dataset_specs, Study
 from nianalysis.dataset import DatasetSpec
 
 
 class CoregisteredStudy(Study):
 
-    def registration_pipeline(self, degree_of_freedom=6,
-                              cost_func='mutualinfo',
-                              search_cost_func='mutualinfo', qsform=False,
-                              interpolate=None, supply_matrix=False,
-                              **kwargs):  # @UnusedVariable @IgnorePep8
+    _registration_inputs = ['reference', 'to_register']
+    _registration_outputs = ['registered', 'matrix']
+
+    def registration_pipeline(self, degrees_of_freedom=6,
+                              cost_func='mutualinfo', qsform=False, **kwargs):  # @UnusedVariable @IgnorePep8
         """
         Registers a MR scan to a refernce MR scan using FSL's FLIRT command
 
         Parameters
         ----------
-        degree_of_freedom : int
+        degrees_of_freedom : int
             Number of degrees of freedom used in the registration. Default is
             6 -> affine transformation.
         cost_func : str
             Cost function used for the registration. Can be one of
             'mutualinfo', 'corratio', 'normcorr', 'normmi', 'leastsq',
             'labeldiff', 'bbr'
-        search_cost_func : str
-            Cost function used for the ?? in the registration. Can be one of
-            'mutualinfo', 'corratio', 'normcorr', 'normmi', 'leastsq',
-            'labeldiff', 'bbr'
         qsform : bool
             Whether to use the QS form supplied in the input image header (
             the image coordinates of the FOV supplied by the scanner)
-        interpolation : str
-            Type of interpolation used. Can be one of 'trilinear',
-            'nearestneighbour', 'sinc', 'spline', None
-        supply_matrix : bool
-            Supplied depending on the context (i.e. other sub-studies) in which
-            the registration is performed. If the matrix is supplied by another
-            registration study then it is not output here (the dataset specs
-            should be mapped accordingly), otherwise it is outputted by this
-            pipeline.
         """
-        inputs = ['reference', 'to_register']
-        outputs = ['registered']
-        if supply_matrix:
-            inputs.append('matrix')
-        else:
-            outputs.append('matrix')
+
         pipeline = self._create_pipeline(
-            name='segmentation',
-            inputs=inputs,
-            outputs=outputs,
+            name='registration',
+            inputs=self._registration_inputs,
+            outputs=self._registration_outputs,
             description="Registers a MR scan against a reference image",
             options=dict(
-                degree_of_freedom=degree_of_freedom, cost_func=cost_func,
-                search_cost_func=search_cost_func, qsform=qsform,
-                interpolate=interpolate),
+                degree_of_freedom=degrees_of_freedom, cost_func=cost_func,
+                qsform=qsform),
             requirements=[fsl5_req],
             citations=[fsl_cite],
-            approx_runtime=1)
+            approx_runtime=5)
         flirt = pe.Node(interface=FLIRT(), name='flirt')
-        flirt.inputs.apply_xfm = (interpolate is not None)
-        flirt.inputs.interp = interpolate
-        flirt.inputs.dof = degree_of_freedom
+        # Set registration options
+
+        flirt.inputs.dof = degrees_of_freedom
         flirt.inputs.cost = cost_func
-        flirt.inputs.cost_func = search_cost_func
+        flirt.inputs.cost_func = cost_func
         flirt.inputs.uses_qform = qsform
         # Connect inputs
         pipeline.connect_input('to_register', flirt, 'in_file')
         pipeline.connect_input('reference', flirt, 'reference')
         # Connect outputs
         pipeline.connect_output('registered', flirt, 'out_file')
-        pipeline.connect_ouptut('matrix', flirt, 'out_matrix_file')
         # Connect matrix
-        if supply_matrix:
-            pipeline.connect_input('matrix', flirt, 'in_matrix_file')
-        else:
-            pipeline.connect_output('matrix', flirt, 'out_matrix_file')
+        self._connect_matrix(pipeline, flirt)
         pipeline.assert_connected()
         return pipeline
+
+    def _connect_matrix(self, pipeline, flirt):
+        pipeline.connect_output('matrix', flirt, 'out_matrix_file')
 
     _dataset_specs = set_dataset_specs(
         DatasetSpec('reference', nifti_gz_format),
         DatasetSpec('to_register', nifti_gz_format),
         DatasetSpec('registered', nifti_gz_format, registration_pipeline),
-        DatasetSpec('matrix', nifti_gz_format, registration_pipeline))
+        DatasetSpec('matrix', text_matrix_format, registration_pipeline))
+
+
+class CoregisteredToMatrixStudy(CoregisteredStudy):
+    """
+    Like CoregisteredStudy but in this study the registration matrix is
+    supplied as an input (typically by another sub-study)
+    """
+
+    _registration_inputs = ['reference', 'to_register', 'matrix']
+    _registration_outputs = ['registered']
+
+    def registration_pipeline(self, interpolate='trilinear', **kwargs):  # @UnusedVariable @IgnorePep8
+        """
+        Registers a MR scan to a refernce MR scan using FSL's FLIRT command
+        using an existing registration matrix
+
+        Parameters
+        ----------
+        interpolate : str
+            Type of interpolation used. Can be one of 'trilinear',
+            'nearestneighbour', 'sinc', 'spline', None. If None no registration
+            is performed.
+
+        (NB: see CoregisteredStudy.registration_pipeline for remaining params)
+        """
+        pipeline = super(
+            CoregisteredToMatrixStudy, self).registration_pipeline(**kwargs)
+        pipeline.node('flirt').inputs.apply_xfm = (interpolate is not None)
+        if interpolate is not None:
+            pipeline.node('flirt').inputs.interp = interpolate
+        pipeline.options['interpolate'] = interpolate
+        return pipeline
+
+    def _connect_matrix(self, pipeline, flirt):
+        pipeline.connect_input('matrix', flirt, 'in_matrix_file')
+
+    _dataset_specs = set_dataset_specs(
+        DatasetSpec('reference', nifti_gz_format),
+        DatasetSpec('to_register', nifti_gz_format),
+        DatasetSpec('matrix', text_matrix_format),
+        DatasetSpec('registered', nifti_gz_format, registration_pipeline))
---
+++ test/unittests/study/mri/test_coregistered.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+from nipype import config
+config.enable_debug_mode()
+import os.path  # @IgnorePep8
+from nianalysis.dataset import Dataset  # @IgnorePep8
+from nianalysis.data_formats import nifti_gz_format, text_matrix_format  # @IgnorePep8
+from nianalysis.study.mri.coregistered import (  # @IgnorePep8
+    CoregisteredStudy, CoregisteredToMatrixStudy)
+from nianalysis.archive.local import LocalArchive  # @IgnorePep8
+if __name__ == '__main__':
+    from nianalysis.testing import DummyTestCase as TestCase  # @IgnorePep8 @UnusedImport
+else:
+    from nianalysis.testing import BaseImageTestCase as TestCase  # @IgnorePep8 @Reimport
+
+
+class TestCoregisteredStudy(TestCase):
+
+    DATASET_NAME = 'coreg'
+    PROJECT_NAME = 'coregistration'
+
+    def test_registration(self):
+        self._remove_generated_files(self.PROJECT_NAME)
+        study = CoregisteredStudy(
+            name=self.DATASET_NAME,
+            project_id=self.PROJECT_NAME,
+            archive=LocalArchive(self.ARCHIVE_PATH),
+            input_datasets={
+                'to_register': Dataset('t2', nifti_gz_format),
+                'reference': Dataset('t1', nifti_gz_format)})
+        pipeline = study.registration_pipeline()
+        pipeline.run()
+        self.assertTrue(
+            os.path.exists(os.path.join(
+                self._session_dir(self.PROJECT_NAME),
+                'registered{}'.format(nifti_gz_format.extension))),
+            "'registered{}' was not created by registration pipeline"
+            .format(nifti_gz_format.extension))
+        self.assertTrue(
+            os.path.exists(os.path.join(
+                self._session_dir(self.PROJECT_NAME),
+                'matrix{}'.format(text_matrix_format.extension))),
+            "'matrix{}' was not created by registration pipeline"
+            .format(text_matrix_format.extension))
+        self._remove_generated_files(self.PROJECT_NAME)
+        # Temporary
+        os.remove(os.path.join(
+            self._session_dir(self.PROJECT_NAME), 'registered.nii.gz'))
+        to_matrix_study = CoregisteredToMatrixStudy(
+            name=self.DATASET_NAME + '_to_matrix',
+            project_id=self.PROJECT_NAME,
+            archive=LocalArchive(self.ARCHIVE_PATH),
+            input_datasets={
+                'to_register': Dataset('t2', nifti_gz_format),
+                'reference': Dataset('t1', nifti_gz_format),
+                'matrix': Dataset('matrix', text_matrix_format)})
+        to_matrix_study.registration_pipeline().run()
+        self.assertTrue(
+            os.path.exists(os.path.join(
+                self._session_dir(self.PROJECT_NAME),
+                'registered{}'.format(nifti_gz_format.extension))),
+            "'registered{}' was not created by registration pipeline"
+            .format(nifti_gz_format.extension))
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--tester', default='diffusion', type=str,
+                        help="Which tester to run the test from")
+    parser.add_argument('--test', default='registration', type=str,
+                        help="Which test to run")
+    args = parser.parse_args()
+    tester = TestCoregisteredStudy()
+    tester.setUp()
+    try:
+        getattr(tester, 'test_' + args.test)()
+    except AttributeError as e:
+        if str(e) == 'test_' + args.test:
+            raise Exception("Unrecognised test '{}' for '{}' tester"
+                            .format(args.test, args.tester))
+        else:
+            raise
+    finally:
+        tester.tearDown()
