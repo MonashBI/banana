d4f47a416195ff8da56cb2f949b67dca9c83ddce
Tom Close <tclose@oist.jp>
2016-12-09 17:05:44 +1100

    derived brain mask to use different parameters for T1 and T2 studies

--- nianalysis/study/mri/structural/t2.py
+++ nianalysis/study/mri/structural/t2.py
@@ -1,12 +1,15 @@
 from itertools import chain
 from nianalysis.study.base import set_dataset_specs
-from nianalysis.dataset import DatasetSpec
-from nianalysis.data_formats import nifti_gz_format
 from ..base import MRStudy
 
 
 class T2Study(MRStudy):
 
+    def brain_mask_pipeline(self, robust=True, threshold=0.5,
+                            reduce_bias=False, **kwargs):
+        return super(T2Study, self).brain_mask_pipeline(
+            robust=robust, threshold=threshold, reduce_bias=reduce_bias,
+            **kwargs)
+
     _dataset_specs = set_dataset_specs(
-        DatasetSpec('t2', nifti_gz_format),
-        inherit_from=chain(MRStudy.generated_dataset_specs()))
+        inherit_from=chain(MRStudy.dataset_specs()))
