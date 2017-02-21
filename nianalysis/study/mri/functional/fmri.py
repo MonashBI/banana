a1601bff0de3f78274b58a3a2c5a12ec9481974e
Francesco Sforazzini <francesco.sforazzini@monash.edu>
2017-02-21 16:25:43 +1100

    implemented feat pipeline in FunctionalMRI study

--- nianalysis/study/mri/functional/fmri.py
+++ nianalysis/study/mri/functional/fmri.py
@@ -1,14 +1,77 @@
 from nipype.pipeline import engine as pe
-from nipype.interfaces import fsl
+from nipype.interfaces.fsl.model import FEAT
+from nipype.interfaces.fsl.epi import PrepareFieldmap
+from nipype.interfaces.fsl.preprocess import BET
+from nipype.interfaces.fsl.utils import SwapDimensions
+from nianalysis.interfaces.fsl import MelodicL1FSF
 from nianalysis.dataset import DatasetSpec
 from nianalysis.study.base import set_dataset_specs
 from ..base import MRIStudy
-from nianalysis.requirements import Requirement
+from nianalysis.requirements import Requirement, fsl5_req
 from nianalysis.citations import fsl_cite
-from nianalysis.data_formats import nifti_gz_format
+from nianalysis.data_formats import nifti_gz_format, zip_format
 
 
 class FunctionalMRIStudy(MRIStudy):
+
+    def feat_pipeline(self, **options):
+        pipeline = self._create_pipeline(
+            name='feat',
+            inputs=['field_map_mag', 'field_map_phase', 't1', 'rs_fmri',
+                    'rs_fmri_ref'],
+            outputs=['feat_dir'],
+            description="MELODIC Level 1",
+            default_options={'brain_thresh_percent': 5},
+            version=1,
+            requirements=[fsl5_req],
+            citations=[fsl_cite],
+            approx_runtime=60,
+            options=options)
+        swap_dims = pe.Node(interface=SwapDimensions(), name="swap_dims")
+        swap_dims.inputs.new_dims = ('LR', 'PA', 'IS')
+        pipeline.connect_input('t1', swap_dims, 'in_file')
+
+        bet = pe.Node(interface=BET(), name="bet")
+        bet.inputs.frac = 0.2
+        bet.inputs.reduce_bias = True
+        pipeline.connect_input('field_map_mag', bet, 'in_file')
+
+        bet2 = pe.Node(interface=BET(), name="bet2")
+        bet2.inputs.frac = 0.2
+        bet2.inputs.reduce_bias = True
+        pipeline.connect(swap_dims, "out_file", bet2, "in_file")
+        create_fmap = pe.Node(interface=PrepareFieldmap(), name="prepfmap")
+#       create_fmap.inputs.in_magnitude = fmap_mag[0]
+
+        create_fmap.inputs.delta_TE = 2.46
+        pipeline.connect(bet, "out_file", create_fmap, "in_magnitude")
+        pipeline.connect_input('field_map_phase', create_fmap, 'in_phase')
+
+        mel = MelodicL1FSF()
+        mel.inputs.brain_thresh = pipeline.option('brain_thresh_percent')
+        ml1 = pe.Node(interface=mel, name="mL1FSF")
+        ml1.inputs.tr = 0.754
+        ml1.inputs.dwell_time = 0.39
+        ml1.inputs.te = 21
+        ml1.inputs.unwarp_dir = "x"
+        ml1.inputs.sfwhm = 3
+        pipeline.connect_input('rs_fmri', ml1, 'fmri')
+        pipeline.connect_input('rs_fmri_ref', ml1, 'fmri_ref')
+#        ml1.inputs.fmap_mag = [0]
+#        ml1.inputs.structural = struct[0]
+        ml1.inputs.high_pass = 75
+        pipeline.connect(create_fmap, "out_fieldmap", ml1, "fmap")
+        pipeline.connect(bet, "out_file", ml1, "fmap_mag")
+        pipeline.connect(bet2, "out_file", ml1, "structural")
+        #ml1.inputs.output_dir = output+subject+"/T1/melodic.ica"
+        # fix next
+        feat = pe.Node(interface=FEAT(), name="featL1")
+        feat.inputs.terminal_output = 'none'
+        pipeline.connect(ml1, 'fsf_file', feat, 'fsf_file')
+        pipeline.connect_output('feat_dir', feat, 'feat_dir')
+
+        pipeline.assert_connected()
+        return pipeline
 
     def melodic_pipeline(self, **options):  # @UnusedVariable
         """
@@ -39,4 +102,9 @@ class FunctionalMRIStudy(MRIStudy):
         raise NotImplementedError
 
     _dataset_specs = set_dataset_specs(
-        DatasetSpec('primary', nifti_gz_format))
+        DatasetSpec('field_map_mag', nifti_gz_format),
+        DatasetSpec('field_map_phase', nifti_gz_format),
+        DatasetSpec('t1', nifti_gz_format),
+        DatasetSpec('rs_fmri', nifti_gz_format),
+        DatasetSpec('rs_fmri_ref', nifti_gz_format),
+        DatasetSpec('feat_dir', zip_format, feat_pipeline))
