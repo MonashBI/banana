a544302faed535a80b617e4c9c1a89af7ae94123
Tom Close <tclose@oist.jp>
2017-02-21 15:10:55 +1100

    updated test_qsm and debugged auto unzipping

--- test/unittests/study/mri/structural/test_qsm.py
+++ test/unittests/study/mri/structural/test_qsm.py
@@ -1,16 +1,10 @@
-import shutil
-import errno
 import logging  # @IgnorePep8
 from nipype import config
 config.enable_debug_mode()
-import xnat  # @IgnorePep8
-import os.path  # @IgnorePep8
 from nianalysis.dataset import Dataset  # @IgnorePep8
-from nianalysis.archive.local import LocalArchive  # @IgnorePep8
-from nianalysis.testing import test_data_dir  # @IgnorePep8
 from nianalysis.testing import PipelineTeseCase as TestCase  # @IgnorePep8 @Reimport
 
-from nianalysis.data_formats import coils_zip_format  # @IgnorePep8
+from nianalysis.data_formats import zip_format  # @IgnorePep8
 from nianalysis.study.mri.structural.t2star_kspace import T2StarKSpaceStudy  # @IgnorePep8
 
 logger = logging.getLogger('NiAnalysis')
@@ -19,8 +13,8 @@ logger = logging.getLogger('NiAnalysis')
 class TestQSM(TestCase):
 
     def test_qsm_pipeline(self):
-        study self.create_study(
-        	T2StarKSpaceStudy, 'qsm', input_datasets={
-                't2starkspace': Dataset('swi_coils', coils_zip_format)})
+        study = self.create_study(
+            T2StarKSpaceStudy, 'qsm', input_datasets={
+                't2starkspace': Dataset('swi_coils', zip_format)})
         study.qsm_pipeline().run(work_dir=self.work_dir)
         self.assertDatasetCreated('qsm.nii.gz')
