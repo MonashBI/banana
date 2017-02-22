818e8f1a33d90443564d06d21f2d68ea370b857c
Tom Close <tclose@oist.jp>
2017-02-23 09:38:25 +1100

    cleaned up test_t1t2

--- test/unittests/study/mri/structural/test_t1t2.py
+++ test/unittests/study/mri/structural/test_t1t2.py
@@ -1,18 +1,13 @@
-#!/usr/bin/env python
-
 from nipype import config
 config.enable_debug_mode()
 from nianalysis.dataset import Dataset  # @IgnorePep8
 from nianalysis.study.mri.structural.t1t2 import T1T2Study # @IgnorePep8
 from nianalysis.data_formats import (  # @IgnorePep8
     nifti_gz_format)
-if __name__ == '__main__':
-    from nianalysis.testing import DummyTestCase as TestCase  # @IgnorePep8 @UnusedImport
-else:
-    from nianalysis.testing import BaseTestCase as TestCase  # @IgnorePep8 @Reimport
+from nianalysis.testing import BaseTestCase  # @IgnorePep8
 
 
-class TestT1T2Study(TestCase):
+class TestT1T2Study(BaseTestCase):
 
     def test_t2_registration_pipeline(self):
         study = self.create_study(
@@ -22,13 +17,13 @@ class TestT1T2Study(TestCase):
         study.t2_registration_pipeline().run(work_dir=self.work_dir)
         self.assertDatasetCreated('t2_coreg.nii.gz', study.name)
 
-#     def test_freesurfer_pipeline(self):
-#         study = self.create_study(
-#             T1T2Study, 'freesurfer', input_datasets={
-#                 't1': Dataset('mprage', nifti_format),
-#                 't2': Dataset('flair', nifti_format)})
-#         study.freesurfer_pipeline().run(work_dir=self.work_dir)
-#         self.assertDatasetCreated('fs_recon_all.fs.zip')
+    def test_freesurfer_pipeline(self):
+        study = self.create_study(
+            T1T2Study, 'freesurfer', input_datasets={
+                't1': Dataset('mprage', nifti_gz_format),
+                't2': Dataset('flair', nifti_gz_format)})
+        study.freesurfer_pipeline().run(work_dir=self.work_dir)
+        self.assertDatasetCreated('fs_recon_all.fs.zip')
 
     def test_brain_mask_pipelines(self):
         study = self.create_study(
@@ -43,7 +38,3 @@ class TestT1T2Study(TestCase):
         for fname in ('t1_masked.nii.gz', 't2_masked.nii.gz',
                       'brain_mask.nii.gz', 'manual_wmh_mask_coreg.nii.gz'):
             self.assertDatasetCreated(fname, study.name)
-
-if __name__ == '__main__':
-    tester = TestT1T2Study()
-    tester.test_freesurfer_pipeline()
