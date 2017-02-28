6d4f0883daf24a00a25c5d563458906f8178b074
Francesco Sforazzini <francesco.sforazzini@monash.edu>
2017-02-28 16:42:00 +1100

    added test fix pipeline

--- test/unittests/study/mri/functional/test_fmri.py
+++ test/unittests/study/mri/functional/test_fmri.py
@@ -10,21 +10,21 @@ from nianalysis.testing import BaseTestCase  # @IgnorePep8 @Reimport
 
 class TestFMRI(BaseTestCase):
 
-    def test_feat(self):
-        study = self.create_study(
-            FunctionalMRIStudy, 'feat', input_datasets={
-                'field_map_mag': Dataset('field_map_mag', nifti_gz_format),
-                'field_map_phase': Dataset('field_map_phase', nifti_gz_format),
-                't1': Dataset('mprage', nifti_gz_format),
-                'rs_fmri': Dataset('rs_fmri', nifti_gz_format),
-                'rs_fmri_ref': Dataset('rs_fmri_ref', nifti_gz_format)})
-        study.feat_pipeline().run(work_dir=self.work_dir)
-        self.assertDatasetCreated('feat_dir.zip', study.name)
-
-#     def test_fix(self):
+#     def test_feat(self):
 #         study = self.create_study(
-#             FunctionalMRIStudy, 'fix', input_datasets={
-#                 'feat_dir': Dataset('feat_dir', zip_format),
-#                 'train_data': Dataset('train_data', rdata_format)})
-#         study.fix_pipeline().run(work_dir=self.work_dir)
-#         self.assertDatasetCreated('cleaned_file.nii.gz', study.name)
+#             FunctionalMRIStudy, 'feat', input_datasets={
+#                 'field_map_mag': Dataset('field_map_mag', nifti_gz_format),
+#                 'field_map_phase': Dataset('field_map_phase', nifti_gz_format),
+#                 't1': Dataset('mprage', nifti_gz_format),
+#                 'rs_fmri': Dataset('rs_fmri', nifti_gz_format),
+#                 'rs_fmri_ref': Dataset('rs_fmri_ref', nifti_gz_format)})
+#         study.feat_pipeline().run(work_dir=self.work_dir)
+#         self.assertDatasetCreated('feat_dir.zip', study.name)
+
+    def test_fix(self):
+        study = self.create_study(
+            FunctionalMRIStudy, 'fix', input_datasets={
+                'feat_dir': Dataset('feat_dir', zip_format),
+                'train_data': Dataset('train_data', rdata_format)})
+        study.fix_pipeline().run(work_dir=self.work_dir)
+        self.assertDatasetCreated('cleaned_file.nii.gz', study.name)
