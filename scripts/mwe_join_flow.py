1bb6dca8b7a13a58f146fe5b5d321957907cb2a9
Tom Close <tclose@oist.jp>
2017-03-01 17:08:22 +1100

    almost there with debugging linked pipelines. Also cleaned up the iterations structure of subjects/sessions to make it more flexible, robust and readable.

--- scripts/mwe_join_flow.py
+++ scripts/mwe_join_flow.py
@@ -28,6 +28,16 @@ class EInputSpec(TraitedSpec):
     in_files = traits.List(traits.Str())
 
 
+class EOutputSpec(TraitedSpec):
+
+    out_files = traits.List(traits.Str())
+
+
+class FInputSpec(TraitedSpec):
+
+    in_files_list = traits.List(traits.List(traits.Str()))
+
+
 class DummyInterface(BaseInterface):
 
     input_spec = InputSpec
@@ -58,6 +68,20 @@ class D(DummyInterface):
 class E(BaseInterface):
 
     input_spec = EInputSpec
+    output_spec = EOutputSpec
+
+    def _run_interface(self, runtime):
+        return runtime
+
+    def _list_outputs(self):
+        outputs = self.output_spec().get()
+        outputs['out_files'] = self.inputs.in_files
+        return outputs
+
+
+class F(BaseInterface):
+
+    input_spec = FInputSpec
 
     def _run_interface(self, runtime):
         return runtime
@@ -69,15 +93,18 @@ b = pe.Node(interface=B(), name="b")
 b.iterables = ("m", [1, 2])
 c = pe.Node(interface=C(), name="c")
 d = pe.Node(interface=D(), name="d")
-d.itersource = ("b", "m")
-d.iterables = [("n", {1: [3, 4], 2: [5, 6]})]
+# d.itersource = ("b", "m")
+d.iterables = ("n", [1, 2])
 my_workflow = pe.Workflow(name="my_workflow", base_dir=os.getcwd())
 my_workflow.connect([(a, b, [('out_file', 'in_file')]),
                      (b, c, [('out_file', 'in_file')]),
                      (c, d, [('out_file', 'in_file')])])
-e = pe.JoinNode(interface=E(), joinsource="d",
+e = pe.JoinNode(interface=E(), joinsource="b",
                 joinfield="in_files", name="e")
 my_workflow.connect(d, 'out_file',
                     e, 'in_files')
-
+f = pe.JoinNode(interface=F(), joinsource="d",
+                joinfield="in_files_list", name="f")
+my_workflow.connect(e, 'out_files',
+                    f, 'in_files_list')
 my_workflow.run()
