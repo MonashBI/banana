a7fa76cf76083d43fae976dbcf9f846e1ece87f2
Phillip Ward <phillip.ward@monash.edu>
2017-05-31 16:50:47 +1000

    QSM DE

--- nianalysis/interfaces/resources/matlab/qsm/Prepare_Masks.m
+++ nianalysis/interfaces/resources/matlab/qsm/Prepare_Masks.m
@@ -1,6 +1,10 @@
-function Prepare_Masks( inDir, maskFile, outDir, nCoils)
+function Prepare_Masks( inDir, maskFile, outDir, nCoils, echoId)
 %PREPARE_MASKS
 %   Coil specific and whole-brain masks using FSL-BET
+
+if nargin<5
+    echoId=1;
+end
 
 % Spherical structure element
 SE = fspecial3('ellipsoid',[11 11 11]);
@@ -12,7 +16,7 @@ mask = imdilate(mask,SE>0);
 
 %Generate coil specific masks by thresholding magnitude image
 for i=0:(nCoils-1)
-    inMag = load_nii([inDir '/Raw_Coil_' num2str(i) '_1_MAGNITUDE.nii.gz']);
+    inMag = load_nii([inDir '/Raw_Coil_' num2str(i) '_' num2str(echoId) '_MAGNITUDE.nii.gz']);
     
     % Blur to remove tissue based contrast
     outVol = convn(inMag.img,SE,'same');
---
+++ nianalysis/interfaces/resources/matlab/qsm/QSM_DualEcho.m
@@ -0,0 +1,55 @@
+function QSM_DualEcho( inDir, maskFile, outDir )
+
+% Add libraries for nifti and STI suite
+addpath(genpath('/data/project/Phil/ASPREE_QSM/scripts/'))
+
+% Prepare directory structure
+phaseFile = [outDir '/QSM/Raw_PHASE.nii.gz'];
+newMaskFile = [outDir '/QSM/PhaseMask.nii.gz'];
+unwrapFile = [outDir '/QSM/Unwrapped.nii.gz'];
+tissueFile = [outDir '/QSM/TissuePhase.nii.gz'];
+qsmFile = [outDir '/QSM/QSM.nii.gz'];
+
+% Combine channels
+HIP_ChannelCombination(inDir, outDir, nCoils);
+
+% Load Inputs (Raw phase and mask)
+mask = load_nii(maskFile);
+nii = load_nii(phaseFile);
+
+% Calc params
+params.H = [0 0 1];
+params.voxelsize = nii.hdr.dime.pixdim(2:4);
+params.padsize = [12 12 12];
+params.TE = 14.76; % (22.14-7.38)
+params.B0 = 3;
+
+% Step 1: Unwrap phase
+% Apply Laplacian unwrapping
+[Unwrapped_Phase, ~]=MRPhaseUnwrap(nii.img,'voxelsize',params.voxelsize,'padsize',params.padsize);
+
+% Save Intermediate Results
+nii.img = Unwrapped_Phase;
+save_nii(nii,unwrapFile);
+
+% Step 2: Remove background phase
+% Apply V-Shape             
+[TissuePhase,NewMask]=V_SHARP(Unwrapped_Phase,mask.img>0,'voxelsize',params.voxelsize);
+
+% Save Intermediate Results
+nii.img = TissuePhase;
+save_nii(nii,tissueFile);
+nii.img = NewMask<0;
+save_nii(nii,newMaskFile);
+
+% Step 3: Reconstruct QSM
+[Susceptibility]= QSM_iLSQR(TissuePhase,NewMask>0,'params',params);
+nii.img = Susceptibility;
+
+% Save QSM
+save_nii(nii,qsmFile);
+
+end
+   
+   
+
