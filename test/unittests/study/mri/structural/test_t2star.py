635a14f29bfe21ec94980f9b0d317d193d412796
Phillip Ward <phillip.ward@monash.edu>
2017-05-31 16:50:47 +1000

    QSM DE

--- test/unittests/study/mri/structural/test_t2star.py
+++ test/unittests/study/mri/structural/test_t2star.py
@@ -26,9 +26,28 @@ class TestQSM(TestCase):
     def test_qsm_de_pipeline(self):
         study = self.create_study(
             T2StarStudy, 'qsm', input_datasets={
+<<<<<<< HEAD
                 'coils': Dataset('swi_coils_de', zip_format)})
         study.qsm_de_pipeline().run(work_dir=self.work_dir)
         for fname in ('qsm.nii.gz', 'tissue_phase.nii.gz',
                       'tissue_mask.nii.gz', 'qsm_mask.nii.gz'):
             self.assertDatasetCreated(dataset_name=fname,
+=======
+                'coils': Dataset('swi_coils', zip_format)})
+        study.qsm_pipeline().run(work_dir=self.work_dir)
+        for fname in ('qsm.nii.gz', 'tissue_phase.nii.gz',
+                      'tissue_mask.nii.gz', 'qsm_mask.nii.gz'):
+            self.assertDatasetCreated(dataset_name=fname,
+                                      study_name=study.name)
+            
+            
+    def test_qsm_de_pipeline(self):
+        study = self.create_study(
+            T2StarStudy, 'qsm', input_datasets={
+                'coils': Dataset('swi_coils_de', zip_format)})
+        study.qsm_de_pipeline().run(work_dir=self.work_dir)
+        for fname in ('qsm.nii.gz', 'tissue_phase.nii.gz',
+                      'tissue_mask.nii.gz', 'qsm_mask.nii.gz'):
+            self.assertDatasetCreated(dataset_name=fname,
+>>>>>>> QSM DE
                                       study_name=study.name)        
