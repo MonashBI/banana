c18d8b52460ac4ad9c81e3144114064ae52c4cde
Phillip Ward <phillip.ward@monash.edu>
2017-06-19 15:46:43 +1000

    Working DualEcho code, adjusted memory for slurm

--- nianalysis/interfaces/resources/matlab/qsm/HIP_ChannelCombination.m
+++ nianalysis/interfaces/resources/matlab/qsm/HIP_ChannelCombination.m
@@ -6,33 +6,30 @@ if nargin<3
     nCoils=32;
 end
 
-pha = [];
-mag = [];
+hip = [];
+sumMag = [];
+
 for i=1:nCoils
-    pNii = load_nii([inDir '/Raw_Coil_' num2str(i-1) '_1_PHASE.nii.gz']);
-    mNii = load_nii([inDir '/Raw_Coil_' num2str(i-1) '_1_MAGNITUDE.nii.gz']);
+    pha1 = load_untouch_nii([inDir '/Raw_Coil_' num2str(i-1) '_1_PHASE.nii.gz']);
+    mag1 = load_untouch_nii([inDir '/Raw_Coil_' num2str(i-1) '_1_MAGNITUDE.nii.gz']);
     
-    if isempty(pha)
-        pha = zeros([size(pNii.img) nCoils 2]);
-        mag = zeros([size(pNii.img) nCoils 2]);
+    pha2 = load_untouch_nii([inDir '/Raw_Coil_' num2str(i-1) '_2_PHASE.nii.gz']);
+    mag2 = load_untouch_nii([inDir '/Raw_Coil_' num2str(i-1) '_2_MAGNITUDE.nii.gz']);
+    
+    if isempty(hip)
+        hip = zeros(size(pha1.img));
+        sumMag = zeros(size(pha1.img));
     end
     
-    pha(:,:,:,i,1) = pNii.img; %#ok<AGROW>
-    mag(:,:,:,i,1) = mNii.img; %#ok<AGROW>
-    
-    pNii = load_nii([inDir '/Raw_Coil_' num2str(i-1) '_2_PHASE.nii.gz']);
-    mNii = load_nii([inDir '/Raw_Coil_' num2str(i-1) '_2_MAGNITUDE.nii.gz']);
-    
-    pha(:,:,:,i,2) = pNii.img; %#ok<AGROW>
-    mag(:,:,:,i,2) = mNii.img; %#ok<AGROW>
-    
+    hip = hip + squeeze(mag1.img.*mag2.img.*exp(-1i.*(pha1.img-pha2.img)));
+    sumMag = sumMag + mag1.img.*mag2.img;
 end
 
-hipInner = squeeze(mag(:,:,:,:,1).*mag(:,:,:,:,2).*exp(-1i.*(pha(:,:,:,:,1)-pha(:,:,:,:,2))));
-hip = sum(hipInner,4);
+pha1.img = angle(hip);
+mag1.img = abs(hip);
 
-pNii.img = angle(hip);
-mNii.img = abs(hip);
+save_untouch_nii(pha1,[outDir '/Raw_PHASE.nii.gz']);
+save_untouch_nii(mag1,[outDir '/Raw_MAGNITUDE.nii.gz']);
 
-save_nii(pNii,[outDir '/Raw_PHASE.nii.gz']);
-save_nii(mNii,[outDir '/Raw_MAGNITUDE.nii.gz']);
+mag1.img = mag1.img./sumMag;
+save_untouch_nii(mag1,[outDir '/Raw_Q.nii.gz']);
