3e6858ba48a1e18b050943bac1ab5626932eb9cb
Phillip Ward <phillip.ward@monash.edu>
2017-07-07 09:25:05 +1000

    Added and tested ANTs code. QSM recon coupling unsuccessful.

--- test/unittests/study/mri/structural/test_t2star.py
+++ test/unittests/study/mri/structural/test_t2star.py
@@ -4,7 +4,7 @@ config.enable_debug_mode()
 from nianalysis.dataset import Dataset  # @IgnorePep8
 from nianalysis.testing import BaseTestCase as TestCase  # @IgnorePep8 @Reimport
 
-from nianalysis.data_formats import zip_format  # @IgnorePep8
+from nianalysis.data_formats import zip_format, nifti_gz_format, text_matrix_format  # @IgnorePep8
 from nianalysis.study.mri.structural.t2star import T2StarStudy  # @IgnorePep8
 
 logger = logging.getLogger('NiAnalysis')
@@ -12,22 +12,54 @@ logger = logging.getLogger('NiAnalysis')
 
 class TestQSM(TestCase):
 
-#    def test_qsm_pipeline(self):
+#    def test_qsm_se_pipeline(self):
 #        study = self.create_study(
-#           T2StarStudy, 'qsm', input_datasets={
+#               T2StarStudy, 'qsm_se', input_datasets={
+#               'coils': Dataset('swi_coils_se', zip_format)})
+#        study.qsm_se_pipeline().run(work_dir=self.work_dir)
+#        for fname in ('qsm.nii.gz', 'tissue_phase.nii.gz',
+#                      'tissue_mask.nii.gz', 'qsm_mask.nii.gz'):
+#            self.assertDatasetCreated(dataset_name=fname,
+#                                      study_name=study.name)
+            
+
+#    def test_qsm_pipeline(self):
+#       study = self.create_study(
+#            T2StarStudy, 'qsm', input_datasets={
 #                'coils': Dataset('swi_coils', zip_format)})
 #        study.qsm_pipeline().run(work_dir=self.work_dir)
 #        for fname in ('qsm.nii.gz', 'tissue_phase.nii.gz',
 #                      'tissue_mask.nii.gz', 'qsm_mask.nii.gz'):
 #            self.assertDatasetCreated(dataset_name=fname,
-#                                      study_name=study.name)
-#            
-    def test_qsm_de_pipeline(self):
+#                                      study_name=study.name)        
+
+#    def test_optibet(self):
+#        study = self.create_study(
+#            T2StarStudy, 'optibet', input_datasets={
+#                't1': Dataset('t1', nifti_gz_format)})
+#        study.optiBET().run(work_dir=self.work_dir, plugin='MultiProc')
+#        self.assertDatasetCreated('betted_file.nii.gz', study.name)
+#        self.assertDatasetCreated('betted_mask.nii.gz', study.name)
+        
+#    def test_ants(self):
+#        study = self.create_study(
+#            T2StarStudy, 'optibet', input_datasets={
+#                't2s': Dataset('t2s', nifti_gz_format),
+#                't1': Dataset('t1', nifti_gz_format)})
+#        study.ANTsRegistration().run(work_dir=self.work_dir,
+#                                    plugin='MultiProc')
+#        self.assertDatasetCreated('T2s2T1.nii.gz', study.name)
+#        self.assertDatasetCreated('T2s2T1_mat.mat', study.name)
+#        self.assertDatasetCreated('T12MNI_linear.nii.gz', study.name)
+#        self.assertDatasetCreated('T12MNI_mat.mat', study.name)
+#        self.assertDatasetCreated('T12MNI_warp.nii.gz', study.name)
+#        self.assertDatasetCreated('T12MNI_invwarp.nii.gz', study.name)
+       
+    def test_apply_trans(self):
         study = self.create_study(
-            T2StarStudy, 'qsm', input_datasets={
+            T2StarStudy, 'apply_tfm', input_datasets={
+                't1': Dataset('t1', nifti_gz_format),
+                't2s': Dataset('t2s', nifti_gz_format),
                 'coils': Dataset('swi_coils', zip_format)})
-        study.qsm_pipeline().run(work_dir=self.work_dir)
-        for fname in ('qsm.nii.gz', 'tissue_phase.nii.gz',
-                      'tissue_mask.nii.gz', 'qsm_mask.nii.gz'):
-            self.assertDatasetCreated(dataset_name=fname,
-                                      study_name=study.name)        
+        study.applyTransform().run(work_dir=self.work_dir, plugin='Linear')
+        self.assertDatasetCreated('qsm_in_mni.nii.gz', study.name)
