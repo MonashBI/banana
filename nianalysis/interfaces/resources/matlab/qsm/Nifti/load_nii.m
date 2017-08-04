1d3a092d56e70e023ef1582ac101c88fe8e8c4e1
Phillip Ward <phillip.ward@monash.edu>
2017-08-04 10:35:53 +1000

    Removed rand in load_nii

--- nianalysis/interfaces/resources/matlab/qsm/Nifti/load_nii.m
+++ nianalysis/interfaces/resources/matlab/qsm/Nifti/load_nii.m
@@ -133,8 +133,7 @@ function nii = load_nii(filename, img_idx, dim5_idx, dim6_idx, dim7_idx, ...
       % prefix tempname for MASSIVE
       [~,hostname]=system('hostname');
       if strcmp(hostname(1:2),'m3')
-      	 rng('shuffle')
-         tmpDir = ['/scratch/dq13' tempname '_' num2str(rand(1)*9999)];
+         tmpDir = ['/scratch/dq13' tempname];
       else
          tmpDir = tempname;
       end
@@ -148,7 +147,7 @@ function nii = load_nii(filename, img_idx, dim5_idx, dim6_idx, dim7_idx, ...
          filename2 = [filename2, '.hdr.gz'];
 
          %tmpDir = tempname;
-         mkdir(tmpDir);
+         system(['mkdir ' tmpDir]);
          gzFileName = filename;
 
          filename1 = gunzip(filename1, tmpDir);
@@ -161,7 +160,7 @@ function nii = load_nii(filename, img_idx, dim5_idx, dim6_idx, dim7_idx, ...
          filename2 = [filename2, '.img.gz'];
 
          %tmpDir = tempname;
-         mkdir(tmpDir);
+         system(['mkdir ' tmpDir]);
          gzFileName = filename;
 
          filename1 = gunzip(filename1, tmpDir);
@@ -169,7 +168,7 @@ function nii = load_nii(filename, img_idx, dim5_idx, dim6_idx, dim7_idx, ...
          filename = char(filename1);	% convert from cell to string
       elseif strcmp(filename(end-6:end), '.nii.gz')
          %tmpDir = tempname;
-         mkdir(tmpDir);
+         system(['mkdir ' tmpDir]);
          gzFileName = filename;
          filename = gunzip(filename, tmpDir);
          filename = char(filename);	% convert from cell to string
