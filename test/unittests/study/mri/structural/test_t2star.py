1e7b66d00d90b8f4228b19d6ac85a9c58a769282
Phillip Ward <phillip.ward@monash.edu>
2017-09-07 14:31:16 +1000

    Added bias correction to prepare coils

--- test/unittests/study/mri/structural/test_t2star.py
+++ test/unittests/study/mri/structural/test_t2star.py
@@ -37,12 +37,12 @@ class TestQSM(TestCase):
         study = self.create_study(
             T2StarStudy, 'test_atlas', input_datasets={
                 't1': Dataset('t1', nifti_gz_format),
-                'raw_coils': Dataset('swi_coils_se', zip_format),
+                'raw_coils': Dataset('swi_coils', zip_format),
                 #'opti_betted_T2s_mask': Dataset('test_opti_betted_T2s_mask', nifti_gz_format),
                 #'betted_T2s_mask': Dataset('test_se_betted_T2s_mask', nifti_gz_format),
                 #'betted_T2s': Dataset('test_se_betted_T2s', nifti_gz_format),
-                't2s': Dataset('test_t2s', nifti_gz_format),
-                'opti_betted_T2s': Dataset('test_opti_betted_t2s', nifti_gz_format),
+                #'t2s': Dataset('test_t2s', nifti_gz_format),
+                #'opti_betted_T2s': Dataset('test_opti_betted_t2s', nifti_gz_format),
                 'T2s_to_T1_mat': Dataset('test_T2s_to_T1_mat', text_matrix_format),
                 'SUIT_to_T1_warp': Dataset('test_SUIT_to_T1_warp', nifti_gz_format),
                 'T1_to_SUIT_warp': Dataset('test_T1_to_SUIT_warp', nifti_gz_format),
@@ -51,7 +51,7 @@ class TestQSM(TestCase):
                 'T1_to_MNI_warp': Dataset('test_T1_to_MNI_warp', nifti_gz_format),
                 'T1_to_MNI_mat': Dataset('test_T1_to_MNI_mat', text_matrix_format),
                 't2s_in_mni': Dataset('test_t2s_in_mni', nifti_gz_format),
-                't2s_mni_atlas': Dataset('test_t2s_mni_atlas', nifti_gz_format),
+                't2s_in_mni_initial_atlas': Dataset('test_t2s_mni_atlas', nifti_gz_format),
                 #'T2s_to_MNI_Template_warp': Dataset('test_T2s_to_MNI_Template_warp', nifti_gz_format),
                 #'T2s_to_MNI_Template_mat': Dataset('test_T2s_to_MNI_Template_mat', text_matrix_format),
                 #'T2s_in_MNI_Template': Dataset('test_T2s_in_MNI_Template', nifti_gz_format),
@@ -61,7 +61,7 @@ class TestQSM(TestCase):
                 #'left_dentate_in_qsm': Dataset('test_analysis_left_dentate_in_qsm', nifti_gz_format)
                 })
         #study.t2s_atlas(qsm_num_channels=8, qsm_echo_times=[20], swi_coils_filename='T2swi3d_axial_p2_1.8mm_Coil').run(work_dir=self.work_dir, plugin='MultiProc')
-        study.qsmInMNI().run(work_dir=self.work_dir, plugin='MultiProc')
+        study.prepare_swi_coils().run(work_dir=self.work_dir, plugin='MultiProc')
         self.assertDatasetCreated(dataset_name='T2s_in_MNI_Template.nii.gz', study_name=study.name)
         #self.assertDatasetCreated(multiplicity='per_project',dataset_name='t2s_mni_atlas.nii.gz', study_name=study.name)
         
