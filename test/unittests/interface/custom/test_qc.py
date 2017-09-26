2e4ea33f7fa63b803efddf29075fcba3283f74ea
Tom Close <tom.g.close@gmail.com>
2017-09-26 15:21:15 +1000

    added qc metrics interface and unittest to calculate SNR

---
+++ test/unittests/interface/custom/test_qc.py
@@ -0,0 +1,18 @@
+from nianalysis.testing import BaseTestCase
+import os.path
+import logging
+from nianalysis.nodes import Node
+from nianalysis.interfaces.custom.qc import QCMetrics
+
+logger = logging.getLogger('NiAnalysis')
+
+
+class TestQC(BaseTestCase):
+
+    def test_subtract(self):
+        # Create Zip node
+        metrics = Node(QCMetrics(), name='metrics')
+        metrics.inputs.in_file = os.path.join(self.session_dir,
+                                              '32ch_mprage.nii')
+        out = metrics.run()
+        self.assertEqual(out.outputs.snr, 23.838683913521724)
