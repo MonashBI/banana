e6482aff3394b6acadca1069cbb481f3357f9ca1
Tom Close <tom.g.close@gmail.com>
2017-09-26 16:57:52 +1000

    implemented uniformity and ghost intensity measures

--- nianalysis/interfaces/custom/qc.py
+++ nianalysis/interfaces/custom/qc.py
@@ -15,16 +15,33 @@ class QCMetricsInputSpec(TraitedSpec):
         0.25, mandatory=False, usedefault=True,
         desc=("The threshold used to determine whether a voxel is in the ROI "
               "or not, scaled to the intensity range of the image"))
-    buffer = traits.Tuple(
-        traits.Float(), (0.2, 0.2), mandatory=False, usedefault=True,
-        desc=("The internal and external buffer (as a fraction) subtracted "
-              "and added to the radius when determining the phantom and "
-              "non-phantom masks respectively"))
+    signal_radius = traits.Float(
+        0.8, mandatory=False, usedefault=True,
+        desc=("The radius within which to calculate the signal intensity as a "
+              "fraction of the estimated radius of the phantom"))
+
+    ghost_radius = traits.Tuple(
+        traits.Float(), (1.2, 1.6), mandatory=False, usedefault=True,
+        desc=("The internal and external radii of the region within which to "
+              " calculate the ghost signal as a fraction of the estimated "
+              "radius of the phantom"))
+
+    background_radius = traits.Float(
+        1.6, mandatory=False, usedefault=True,
+        desc=("The internal radius of the region within which to "
+              " calculate the backgrouind signal as a fraction of the "
+              "estimated radius of the phantom"))
 
 
 class QCMetricsOutputSpec(TraitedSpec):
 
     snr = traits.Float(desc='The SNR calculated from the QC phantom')
+
+    uniformity = traits.Float(
+        desc="The uniformity of the signal in the phantom")
+
+    ghost_intensity = traits.Float(
+        desc="The intensity of the ghost region of the image")
 
 
 class QCMetrics(BaseInterface):
@@ -58,25 +75,37 @@ class QCMetrics(BaseInterface):
             np.arange(qc.shape[0]),
             np.arange(qc.shape[1]),
             np.arange(qc.shape[2]))
-        phantom_mask = self._in_volume(x, y, z,
-                                       rad * (1 - self.inputs.buffer[0]),
-                                       extent[2],
-                                       centre)
-        non_phantom_mask = self._in_volume(x, y, z,
-                                           rad * (1 + self.inputs.buffer[1]),
-                                           extent[2],
-                                           centre, invert=True)
-        phantom = qc[phantom_mask]
-        non_phantom = qc[non_phantom_mask]
-        plt_phantom = np.array(qc)
-        plt_non_phantom = np.array(qc)
-        plt_phantom[np.logical_not(phantom_mask)] = 0.0
-        plt_non_phantom[np.logical_not(non_phantom_mask)] = 0.0
-        plt.imshow(plt_phantom[:, :, 100])
-        plt.imshow(plt_non_phantom[:, :, 100])
-        snr = np.sqrt(2) * np.mean(phantom) / np.std(non_phantom)
+        signal_mask = self._in_volume(
+            x, y, z, rad * self.inputs.signal_radius, extent[2], centre)
+        internal_ghost_mask = self._in_volume(
+            x, y, z, rad * self.inputs.ghost_radius[0], extent[2], centre,
+            invert=True)
+        external_ghost_mask = self._in_volume(
+            x, y, z, rad * self.inputs.ghost_radius[1], extent[2], centre)
+        ghost_mask = np.logical_and(internal_ghost_mask, external_ghost_mask)
+        background_mask = self._in_volume(
+            x, y, z, rad * self.inputs.background_radius, extent[2], centre)
+        signal = qc[signal_mask]
+        ghost = qc[ghost_mask]
+        background = qc[background_mask]
+        plt_signal = np.array(qc)
+        plt_ghost = np.array(qc)
+        plt_background = np.array(qc)
+        plt_signal[np.logical_not(signal_mask)] = 0.0
+        plt_ghost[np.logical_not(ghost_mask)] = 0.0
+        plt_ghost[np.logical_not(background_mask)] = 0.0
+        plt.imshow(plt_signal[:, :, 100])
+        plt.imshow(plt_ghost[:, :, 100])
+        plt.imshow(plt_background[:, :, 100])
+        snr = np.sqrt(2) * np.mean(signal) / np.std(background)
+        uniformity = (
+            100.0 * (signal.max() - signal.min()) /
+            (signal.max() + signal.min()))
+        ghost_intensity = 100.0 * np.mean(signal) / np.mean(ghost)
         outputs = self._outputs().get()
         outputs['snr'] = snr
+        outputs['uniformity'] = uniformity
+        outputs['ghost_intensity'] = ghost_intensity
         return outputs
 
     @classmethod
