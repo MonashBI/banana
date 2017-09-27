78fcd67783865eca8f81f8e454c8d93d282056ca
Tom Close <tom.g.close@gmail.com>
2017-09-27 11:33:32 +1000

    debugged and set up test for all QC metrics

--- test/unittests/interface/custom/test_qc.py
+++ test/unittests/interface/custom/test_qc.py
@@ -15,6 +15,6 @@ class TestQC(BaseTestCase):
         metrics.inputs.in_file = os.path.join(self.session_dir,
                                               '32ch_mprage.nii')
         out = metrics.run()
-        self.assertEqual(out.outputs.snr, 23.838683913521724)
-        self.assertEqual(out.outputs.uniformity, 23.838683913521724)
-        self.assertEqual(out.outputs.ghost_intensity, 23.838683913521724)
+        self.assertEqual(out.outputs.snr, 1512.2964134062911)
+        self.assertEqual(out.outputs.uniformity, 39.737991266375545)
+        self.assertEqual(out.outputs.ghost_intensity, 29075.079125151788)
