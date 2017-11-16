59d790cfb0c0596d9f13673e3351e4ea73cd2e79
Tom Close <tom.g.close@gmail.com>
2017-11-16 16:43:38 +1100

    added 'cache' method to XNATArchive class to allow pre-run caching of datasets to avoid timeout errors when running many parallel jobs

---
+++ test/scripts/test_cache.py
@@ -0,0 +1,13 @@
+from nianalysis.data_formats import dicom_format
+from nianalysis.dataset import Dataset
+from nianalysis.archive.xnat import XNATArchive
+
+
+archive = XNATArchive()
+
+archive.cache(
+    'MRH032',
+    [Dataset('t1_mprage_sag_p2_iso_1mm', format=dicom_format),
+     Dataset('t2_tra_tse_320_4mm', format=dicom_format)],
+    subject_ids=['MRH032_{:03}'.format(i) for i in range(1, 20)],
+    visit_ids=['MR01', 'MR03'])
