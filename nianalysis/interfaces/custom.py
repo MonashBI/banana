83086f082abaccc86017413d24ad12949ec2873d
Francesco Sforazzini <francesco.sforazzini@monash.edu>
2018-02-06 15:19:21 +1100

    header extraction implementation

--- nianalysis/interfaces/custom.py
+++ nianalysis/interfaces/custom.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plot
 from sklearn.decomposition import PCA
 import glob
 import shutil
+import dicom
 
 
 class PETdrInputSpec(BaseInterfaceInputSpec):
@@ -245,6 +246,8 @@ class MotionMatCalculation(BaseInterface):
         for f in mm:
             shutil.move(f, out_name)
 
+        return runtime
+
     def gen_motion_mat(self, concat, qform, out_name):
 
         concat_inv = np.linalg.inv(concat)
@@ -261,5 +264,86 @@ class MotionMatCalculation(BaseInterface):
         _, out_name, _ = split_filename(self.inputs.reg_mat)
 
         outputs["motion_mats"] = os.path.abspath(out_name)
+
+        return outputs
+
+
+class DicomHeaderInfoExtractionInputSpec(BaseInterfaceInputSpec):
+
+    dicom_folder = Directory(exists=True, desc='Directory with DICOM files',
+                             mandatory=True)
+    multivol = traits.Bool(desc='Specify whether a scan is 3D or 4D',
+                           default=False)
+
+
+class DicomHeaderInfoExtractionOutputSpec(TraitedSpec):
+
+    tr = traits.Float(desc='Repetition time.')
+    start_time = traits.Str(desc='Scan start time.')
+    real_duration = traits.Str(desc='For 4D files, this will be the number of '
+                               'volumes multiplied by the TR.')
+    tot_duration = traits.Str(
+        desc='Scan duration as extracted from the header.')
+    ped = traits.Str(desc='Phase encoding direction.')
+    phase_offset = traits.Str(desc='Phase offset.')
+
+
+class DicomHeaderInfoExtraction(BaseInterface):
+
+    input_spec = DicomHeaderInfoExtractionInputSpec
+    output_spec = DicomHeaderInfoExtractionOutputSpec
+
+    def _run_interface(self, runtime):
+
+        list_dicom = sorted(glob.glob(self.inputs.dicom_folder+'/*'))
+        multivol = self.inputs.multivol
+        ped = ''
+        phase_offset = ''
+        self.dict_output = {}
+
+        with open(list_dicom[0], 'r') as f:
+            for line in f:
+                if 'TotalScan' in line:
+                    total_duration = line.split('=')[-1].strip()
+                    if not multivol:
+                        real_duration = total_duration
+                elif 'alTR[0]' in line:
+                    tr = float(line.split('=')[-1].strip())/1000000
+                elif 'SliceArray.asSlice[0].dInPlaneRot' and multivol:
+                    phase_offset = float(line.split('=')[-1].strip())
+                    if np.abs(phase_offset) > 1 and np.abs(phase_offset) < 3:
+                        ped = 'ROW'
+                    elif np.abs(phase_offset) < 1 or np.abs(phase_offset) > 3:
+                        ped = 'COL'
+        if multivol:
+            n_vols = len(list_dicom)
+            real_duration = n_vols*tr
+
+        hd = dicom.read_file(list_dicom[0])
+        try:
+            start_time = str(hd.AcquisitionTime)
+        except AttributeError:
+            try:
+                start_time = str(hd.AcquisitionDateTime)[8:]
+            except AttributeError:
+                raise Exception('No acquisition time found for this scan.')
+        self.dict_output['start_time'] = start_time
+        self.dict_output['tr'] = tr
+        self.dict_output['total_duration'] = total_duration
+        self.dict_output['real_duration'] = real_duration
+        self.dict_output['ped'] = ped
+        self.dict_output['phase_offset'] = str(phase_offset)
+
+        return runtime
+
+    def _list_outputs(self):
+        outputs = self._outputs().get()
+
+        outputs["start_time"] = self.dict_output['start_time']
+        outputs["tr"] = self.dict_output['tr']
+        outputs["total_duration"] = self.dict_output['total_duration']
+        outputs["real_duration"] = self.dict_output['real_duration']
+        outputs["ped"] = self.dict_output['ped']
+        outputs["phase_offset"] = self.dict_output['phase_offset']
 
         return outputs
