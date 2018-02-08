a67fa9241262838f94cecc0b2e9e413019b9137d
Francesco Sforazzini <francesco.sforazzini@monash.edu>
2018-02-08 16:23:49 +1100

    added dwi number of directions extraction from the header

--- test/unittests/study/mri/test_mc.py
+++ test/unittests/study/mri/test_mc.py
@@ -3,7 +3,7 @@ from nianalysis.study.mri.structural.t1 import CoregisteredT1Study
 from nianalysis.study.mri.structural.t2 import CoregisteredT2Study
 config.enable_debug_mode()
 from nianalysis.dataset import Dataset  # @IgnorePep8
-from nianalysis.data_formats import nifti_gz_format  # @IgnorePep8
+from nianalysis.data_formats import nifti_gz_format, dicom_format  # @IgnorePep8
 from nianalysis.study.mri.epi import CoregisteredEPIStudy  # @IgnorePep8
 from nianalysis.study.mri.base import MRIStudy
 from nianalysis.testing import BaseTestCase as TestCase  # @IgnorePep8 @Reimport
@@ -33,10 +33,22 @@ class TestMC(TestCase):
 #         study.t1_motion_mat_pipeline().run(work_dir=self.work_dir)
 #         self.assertDatasetCreated('t1_motion_mats', study.name)
 
-    def test_t2_mc(self):
+    def test_hd_info_extraction(self):
         study = self.create_study(
-            CoregisteredT2Study, 't2_reg_study', inputs={
-                't2': Dataset('pd', nifti_gz_format),
-                'reference': Dataset('ref_2', nifti_gz_format)})
-        study.t2_motion_mat_pipeline().run(work_dir=self.work_dir)
-        self.assertDatasetCreated('t2_motion_mats', study.name)
+            MRIStudy, 't2_reg_study', inputs={
+                'dicom_file': Dataset('dwi', dicom_format)})
+        study.header_info_extraction_pipeline().run(work_dir=self.work_dir)
+        self.assertField('tr', 2.45, study.name)
+        self.assertField('start_time', '124629.127500', study.name)
+        self.assertField('tot_duration', '602', study.name)
+        self.assertField('real_duration', '592.9', study.name)
+        self.assertField('ped', '', study.name)
+        self.assertField('phase_offset', '', study.name)
+
+#     def test_t2_mc(self):
+#         study = self.create_study(
+#             CoregisteredT2Study, 't2_reg_study', inputs={
+#                 't2': Dataset('pd', nifti_gz_format),
+#                 'reference': Dataset('ref_2', nifti_gz_format)})
+#         study.t2_motion_mat_pipeline().run(work_dir=self.work_dir)
+#         self.assertDatasetCreated('t2_motion_mats', study.name)
