56782b8ea84958b28240e0d519c17937b685b715
Francesco Sforazzini <francesco.sforazzini@monash.edu>
2018-02-19 08:44:31 +1100

    debugging combined epi study

--- mbianalysis/study/mri/motion_detection.py
+++ mbianalysis/study/mri/motion_detection.py
@@ -7,9 +7,9 @@ from nipype.interfaces.fsl import (ExtractROI, TOPUP, ApplyTOPUP)
 from mbianalysis.interfaces.custom.motion_correction import (
     PrepareDWI, CheckDwiNames, GenTopupConfigFiles)
 from nianalysis.citations import fsl_cite
-from nianalysis.study.base import set_specs
+from nianalysis.study.base import set_data_specs
 from .coregistered import CoregisteredStudy
-from nianalysis.study.multi import MultiStudy
+from nianalysis.study.combined import CombinedStudy
 from mbianalysis.interfaces.custom.motion_correction import (
     MotionMatCalculation, AffineMatrixGeneration)
 from nianalysis.interfaces.converters import Dcm2niix
@@ -19,7 +19,7 @@ from nipype.interfaces.fsl.utils import Merge as fsl_merge
 from .epi import CoregisteredEPIStudy
 
 
-class MotionDetectionStudy(MultiStudy):
+class MotionDetectionStudy(CombinedStudy):
     
     sub_study_specs = {
         'epi1': (CoregisteredEPIStudy, {
@@ -29,7 +29,14 @@ class MotionDetectionStudy(MultiStudy):
             'epi1_qformed': 'epi_qformed',
             'epi1_moco_mat': 'epi_moco_mat',
             'epi1_motion_mats': 'epi_motion_mats',
-            'epi1_preproc': 'epi_preproc'}),
+            'epi1_preproc': 'epi_preproc',
+            'epi1_ref_brain': 'ref_brain',
+            'epi1_ref_preproc': 'ref_preproc',
+            'epi1_ref_brain_mask': 'ref_brain_mask',
+            'epi1_ref_wmseg': 'ref_wmseg',
+            'epi1_reference': 'reference',
+            'epi1_brain': 'epi_brain',
+            'epi1_brain_mask': 'epi_brain_mask'}),
         'epi2': (CoregisteredEPIStudy, {
             'epi2': 'epi',
             'epi2_epireg_mat': 'epi_epireg_mat',
@@ -37,7 +44,14 @@ class MotionDetectionStudy(MultiStudy):
             'epi2_qformed': 'epi_qformed',
             'epi2_moco_mat': 'epi_moco_mat',
             'epi2_motion_mats': 'epi_motion_mats',
-            'epi2_preproc': 'epi_preproc'}),
+            'epi2_preproc': 'epi_preproc',
+            'epi2_ref_brain': 'ref_brain',
+            'epi2_ref_preproc': 'ref_preproc',
+            'epi2_ref_brain_mask': 'ref_brain_mask',
+            'epi2_ref_wmseg': 'ref_wmseg',
+            'epi2_reference': 'reference',
+            'epi2_brain': 'epi_brain',
+            'epi2_brain_mask': 'epi_brain_mask'}),
         'reference': (CoregisteredEPIStudy, {
             'reference': 'reference',
             'ref_preproc': 'ref_preproc',
@@ -45,51 +59,71 @@ class MotionDetectionStudy(MultiStudy):
             'ref_brain_mask': 'ref_brain_mask',
             'ref_wmseg': 'ref_wmseg'})}
     
-    epi1_motion_alignment_pipeline = MultiStudy.translate(
+    epi1_motion_alignment_pipeline = CombinedStudy.translate(
         'epi1', CoregisteredEPIStudy.epi_motion_alignment_pipeline)
     
-    epi1_epireg_pipeline = MultiStudy.translate(
+    epi1_epireg_pipeline = CombinedStudy.translate(
         'epi1', CoregisteredEPIStudy.epireg_pipeline)
     
-    epi1_motion_mat_pipeline = MultiStudy.translate(
+    epi1_motion_mat_pipeline = CombinedStudy.translate(
         'epi1', CoregisteredEPIStudy.epi_motion_mat_pipeline)
     
-    epi1_basic_preproc_pipeline = MultiStudy.translate(
+    epi1_basic_preproc_pipeline = CombinedStudy.translate(
         'epi1', CoregisteredEPIStudy.epi_basic_preproc_pipeline)
     
-    epi1_qform_transform_pipeline = MultiStudy.translate(
+    epi1_qform_transform_pipeline = CombinedStudy.translate(
         'epi1', CoregisteredEPIStudy.epi_qform_transform_pipeline)
     
-    epi2_motion_alignment_pipeline = MultiStudy.translate(
+    epi1_bet_pipeline = CombinedStudy.translate(
+        'epi1', CoregisteredEPIStudy.epi_bet_pipeline)
+    
+    epi2_motion_alignment_pipeline = CombinedStudy.translate(
         'epi2', CoregisteredEPIStudy.epi_motion_alignment_pipeline)
     
-    epi2_epireg_pipeline = MultiStudy.translate(
+    epi2_epireg_pipeline = CombinedStudy.translate(
         'epi2', CoregisteredEPIStudy.epireg_pipeline)
     
-    epi2_motion_mat_pipeline = MultiStudy.translate(
+    epi2_motion_mat_pipeline = CombinedStudy.translate(
         'epi2', CoregisteredEPIStudy.epi_motion_mat_pipeline)
     
-    epi2_basic_preproc_pipeline = MultiStudy.translate(
+    epi2_basic_preproc_pipeline = CombinedStudy.translate(
         'epi2', CoregisteredEPIStudy.epi_basic_preproc_pipeline)
     
-    epi2_qform_transform_pipeline = MultiStudy.translate(
+    epi2_qform_transform_pipeline = CombinedStudy.translate(
         'epi2', CoregisteredEPIStudy.epi_qform_transform_pipeline)
     
-    ref_bet_pipeline = MultiStudy.translate(
+    epi1_ref_bet_pipeline = CombinedStudy.translate(
         'reference', CoregisteredEPIStudy.ref_bet_pipeline)
 
-    ref_segmentation_pipeline = MultiStudy.translate(
+    epi1_ref_segmentation_pipeline = CombinedStudy.translate(
         'reference', CoregisteredEPIStudy.ref_segmentation_pipeline)
 
-    ref_basic_preproc_pipeline = MultiStudy.translate(
+    epi1_ref_basic_preproc_pipeline = CombinedStudy.translate(
         'reference', CoregisteredEPIStudy.ref_basic_preproc_pipeline)
     
-    _data_specs = set_specs(
+    epi2_ref_bet_pipeline = CombinedStudy.translate(
+        'reference', CoregisteredEPIStudy.ref_bet_pipeline)
+
+    epi2_ref_segmentation_pipeline = CombinedStudy.translate(
+        'reference', CoregisteredEPIStudy.ref_segmentation_pipeline)
+
+    epi2_ref_basic_preproc_pipeline = CombinedStudy.translate(
+        'reference', CoregisteredEPIStudy.ref_basic_preproc_pipeline)
+    
+    epi2_bet_pipeline = CombinedStudy.translate(
+        'epi2', CoregisteredEPIStudy.epi_bet_pipeline)
+    
+    _data_specs = set_data_specs(
         DatasetSpec('epi1', nifti_gz_format),
         DatasetSpec('epi2', nifti_gz_format),
-        DatasetSpec('reference', nifti_gz_format),
+        DatasetSpec('epi1_reference', nifti_gz_format),
+        DatasetSpec('epi2_reference', nifti_gz_format),
         DatasetSpec('epi1_preproc', nifti_gz_format,
                     epi1_basic_preproc_pipeline),
+        DatasetSpec('epi1_brain', nifti_gz_format,
+                    epi1_bet_pipeline),
+        DatasetSpec('epi1_brain_mask', nifti_gz_format,
+                    epi1_bet_pipeline),
         DatasetSpec('epi1_qformed', nifti_gz_format,
                     epi1_qform_transform_pipeline),
         DatasetSpec('epi1_qform_mat', text_matrix_format,
@@ -122,9 +156,19 @@ class MotionDetectionStudy(MultiStudy):
                     epi2_motion_alignment_pipeline),
         DatasetSpec('epi2_moco_par', par_format,
                     epi2_motion_alignment_pipeline),
-        DatasetSpec('ref_preproc', nifti_gz_format,
-                    ref_basic_preproc_pipeline),
-        DatasetSpec('ref_brain', nifti_gz_format, ref_bet_pipeline),
-        DatasetSpec('ref_brain_mask', nifti_gz_format,
-                    ref_bet_pipeline),
-        DatasetSpec('ref_wmseg', nifti_gz_format, ref_segmentation_pipeline))
+        DatasetSpec('epi2_brain', nifti_gz_format,
+                    epi2_bet_pipeline),
+        DatasetSpec('epi2_brain_mask', nifti_gz_format,
+                    epi2_bet_pipeline),
+        DatasetSpec('epi1_ref_preproc', nifti_gz_format,
+                    epi1_ref_basic_preproc_pipeline),
+        DatasetSpec('epi1_ref_brain', nifti_gz_format, epi1_ref_bet_pipeline),
+        DatasetSpec('epi1_ref_brain_mask', nifti_gz_format,
+                    epi1_ref_bet_pipeline),
+        DatasetSpec('epi1_ref_wmseg', nifti_gz_format, epi1_ref_segmentation_pipeline),
+        DatasetSpec('epi2_ref_preproc', nifti_gz_format,
+                    epi2_ref_basic_preproc_pipeline),
+        DatasetSpec('epi2_ref_brain', nifti_gz_format, epi2_ref_bet_pipeline),
+        DatasetSpec('epi2_ref_brain_mask', nifti_gz_format,
+                    epi2_ref_bet_pipeline),
+        DatasetSpec('epi2_ref_wmseg', nifti_gz_format, epi2_ref_segmentation_pipeline))
