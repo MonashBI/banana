b8275a219992d55ce446c8058d35c06a2844b7e5
Francesco Sforazzini <francesco.sforazzini@monash.edu>
2018-02-27 15:17:53 +1100

    added new motion detection class for Ian project

--- test/unittests/study/mri/test_mc.py
+++ test/unittests/study/mri/test_mc.py
@@ -9,7 +9,7 @@ from mbianalysis.study.mri.epi import CoregisteredEPIStudy  # @IgnorePep8
 from mbianalysis.study.mri.base import MRIStudy
 from mbianalysis.testing import BaseTestCase as TestCase  # @IgnorePep8 @Reimport
 from mbianalysis.study.mri.structural.diffusion_coreg import CoregisteredDiffusionStudy  # @IgnorePep8
-from mbianalysis.study.mri.motion_detection_ian import MotionDetectionStudy
+from mbianalysis.study.mri.motion_correction_ian_2 import MotionDetectionStudy
 
 class TestMC(TestCase):
 
@@ -142,15 +142,15 @@ class TestMC(TestCase):
 #                 'fm_reference': Dataset('reference', nifti_gz_format)})
 #         study.motion_framing_pipeline().run(work_dir=self.work_dir)
 #         self.assertDatasetCreated('frame_start_times.txt', study.name)
-
-    def test_plot_md(self):
-        study = self.create_study(
-            MotionDetectionStudy, 'plot_md', inputs={
-                'mean_displacement_rc': Dataset('mean_displacement_rc', text_format),
-                'offset_indexes': Dataset('offset_indexes', text_format),
-                'frame_start_times': Dataset('frame_start_times', text_format)})
-        study.plot_mean_displacement_pipeline().run(work_dir=self.work_dir)
-        self.assertDatasetCreated('mean_displacement_plot.png', study.name)
+# 
+#     def test_plot_md(self):
+#         study = self.create_study(
+#             MotionDetectionStudy, 'plot_md', inputs={
+#                 'mean_displacement_rc': Dataset('mean_displacement_rc', text_format),
+#                 'offset_indexes': Dataset('offset_indexes', text_format),
+#                 'frame_start_times': Dataset('frame_start_times', text_format)})
+#         study.plot_mean_displacement_pipeline().run(work_dir=self.work_dir)
+#         self.assertDatasetCreated('mean_displacement_plot.png', study.name)
 #     def test_t2_mc(self):
 #         study = self.create_study(
 #             CoregisteredT2Study, 't2_reg_study', inputs={
@@ -158,3 +158,39 @@ class TestMC(TestCase):
 #                 'reference': Dataset('ref_2', nifti_gz_format)})
 #         study.t2_motion_mat_pipeline().run(work_dir=self.work_dir)
 #         self.assertDatasetCreated('t2_motion_mats', study.name)
+    def test_mc_ian(self):
+        study = self.create_study(
+            MotionDetectionStudy, 'mc_detection_study', inputs={
+                'epi1': Dataset('epi_1_dicom', dicom_format),
+                't1_1': Dataset('t1_1_dicom', dicom_format),
+                't2_1': Dataset('t2_1_dicom', dicom_format),
+                't2_2': Dataset('t2_2_dicom', dicom_format),
+                't2_3': Dataset('t2_3_dicom', dicom_format),
+                't2_4': Dataset('t2_4_dicom', dicom_format),
+                't2_5': Dataset('t2_5_dicom', dicom_format),
+                'dwi_1_main': Dataset('dwi_1_main_dicom', dicom_format),
+                'dwi2ref_1_to_correct': Dataset('dwi2ref_1_dicom',
+                                                     dicom_format),
+                'dwi2ref_1_opposite_to_correct': Dataset('dwi2ref_1_opposite_dicom',
+                                                     dicom_format),
+                'dwi_1_main_ref': Dataset('dwi2ref_1_opposite_dicom',
+                                          dicom_format),
+                'dwi2ref_1_ref': Dataset('dwi2ref_1_opposite_dicom',
+                                              dicom_format),
+                'dwi2ref_1_opposite_ref': Dataset('dwi2ref_1_dicom',
+                                              dicom_format),
+                'ute': Dataset('ute_dicom', dicom_format),
+                'fm': Dataset('fm_dicom', dicom_format),
+                'reference': Dataset('reference_dicom', dicom_format),
+                'epi1_reference': Dataset('reference', nifti_gz_format),
+                't1_1_reference': Dataset('reference', nifti_gz_format),
+                't2_1_reference': Dataset('reference', nifti_gz_format),
+                't2_2_reference': Dataset('reference', nifti_gz_format),
+                't2_3_reference': Dataset('reference', nifti_gz_format),
+                't2_4_reference': Dataset('reference', nifti_gz_format),
+                't2_5_reference': Dataset('reference', nifti_gz_format),
+                'dwi_reference': Dataset('reference', nifti_gz_format),
+                'ute_reference': Dataset('reference', nifti_gz_format),
+                'fm_reference': Dataset('reference', nifti_gz_format)})
+        study.motion_framing_pipeline().run(work_dir=self.work_dir)
+        self.assertDatasetCreated('frame_start_times.txt', study.name)
