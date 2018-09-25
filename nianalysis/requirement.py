924ec84a370ca856b0836a754d465d2db093c923
Tom Close <tom.g.close@gmail.com>
2018-09-25 16:40:00 +1000

    changed requirements to CLIRequirement and MatlabRequirement

--- nianalysis/requirement.py
+++ nianalysis/requirement.py
@@ -1,27 +1,34 @@
-from arcana.requirement import Requirement, matlab_version_split
+from arcana.requirement import (
+    CLIRequirement, MatlabRequirement, matlab_version_split)
 
-
-mrtrix0_3_req = Requirement('mrtrix', min_version=(0, 3, 12),
-                            max_version=(0, 3, 15))
-mrtrix3_req = Requirement('mrtrix', min_version=(3, 0, 0))
-fsl5_req = Requirement('fsl', min_version=(5, 0, 8))
-fsl509_req = Requirement('fsl', min_version=(5, 0, 9),
-                         max_version=(5, 0, 9))
-fsl510_req = Requirement('fsl', min_version=(5, 0, 10),
-                         max_version=(5, 0, 10))
-ants2_req = Requirement('ants', min_version=(2, 0))
-ants19_req = Requirement('ants', min_version=(1, 9))
-spm12_req = Requirement('spm', min_version=(12, 0))
-freesurfer_req = Requirement('freesurfer', min_version=(5, 3))
-matlab2014_req = Requirement('matlab', min_version=(2014, 'a'),
-                             version_split=matlab_version_split)
-matlab2015_req = Requirement('matlab', min_version=(2015, 'a'),
-                             version_split=matlab_version_split)
-noddi_req = Requirement('noddi', min_version=(0, 9)),
-niftimatlab_req = Requirement('niftimatlib', (1, 2))
-dcm2niix_req = Requirement('dcm2niix', min_version=(1, 0, 2))
-fix_req = Requirement('fix', min_version=(1, 0))
-afni_req = Requirement('afni', min_version=(16, 2, 10))
-mricrogl_req = Requirement('mricrogl', min_version=(1, 0, 20170207))
-stir_req = Requirement('stir', min_version=(3, 0))
-c3d_req = Requirement('c3d', min_version=(1, 1, 0))
+mrtrix0_3_req = CLIRequirement('mrtrix', min_version=(0, 3, 12),
+                               max_version=(0, 3, 15), test_cmd='mrconvert')
+mrtrix3_req = CLIRequirement('mrtrix', min_version=(3, 0, 0),
+                             test_cmd='mrconvert')
+fsl5_req = CLIRequirement('fsl', min_version=(5, 0, 8), test_cmd='fsl')
+fsl509_req = CLIRequirement('fsl', min_version=(5, 0, 9),
+                            max_version=(5, 0, 9), test_cmd='fsl')
+fsl510_req = CLIRequirement('fsl', min_version=(5, 0, 10),
+                            max_version=(5, 0, 10), test_cmd='fsl')
+ants2_req = CLIRequirement('ants', min_version=(2, 0),
+                           test_cmd='antsRegistration')
+ants19_req = CLIRequirement('ants', min_version=(1, 9),
+                            test_cmd='antsRegistration')
+spm12_req = MatlabRequirement('spm', min_version=(12, 0),
+                              test_func=None)
+freesurfer_req = CLIRequirement('freesurfer', min_version=(5, 3))
+matlab2014_req = CLIRequirement('matlab', min_version=(2014, 'a'),
+                                version_split=matlab_version_split,
+                                test_cmd='matlab')
+matlab2015_req = CLIRequirement('matlab', min_version=(2015, 'a'),
+                                version_split=matlab_version_split,
+                                test_cmd='matlab')
+noddi_req = MatlabRequirement('noddi', min_version=(0, 9)),
+niftimatlab_req = MatlabRequirement('niftimatlib', (1, 2))
+dcm2niix_req = CLIRequirement('dcm2niix', min_version=(1, 0, 2),
+                              test_cmd='dcm2niix')
+fix_req = CLIRequirement('fix', min_version=(1, 0))
+afni_req = CLIRequirement('afni', min_version=(16, 2, 10))
+mricrogl_req = CLIRequirement('mricrogl', min_version=(1, 0, 20170207))
+stir_req = CLIRequirement('stir', min_version=(3, 0))
+c3d_req = CLIRequirement('c3d', min_version=(1, 1, 0))
