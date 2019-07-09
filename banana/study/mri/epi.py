72c49fbf9c543485863fc16fad1a641c712ce2b9
Tom Close <tom.g.close@gmail.com>
2019-07-09 17:30:29 +1000

    Started to split EpiStudy and EpiSeriesStudy into separate classes

--- banana/study/mri/epi.py
+++ banana/study/mri/epi.py
@@ -54,6 +54,192 @@ class EpiStudy(MriStudy, metaclass=StudyMetaClass):
         ParamSpec('bet_reduce_bias', False),
         ParamSpec('fugue_echo_spacing', 0.000275)]
 
+    def preprocess_pipeline(self, **name_maps):
+
+        if ('field_map_phase' in self.input_names and
+                'field_map_mag' in self.input_names):
+            return self._fugue_pipeline(**name_maps)
+        elif 'reverse_phase' in self.input_names:
+            return self._topup_pipeline(**name_maps)
+        else:
+            return super().preprocess_pipeline(**name_maps)
+
+    def _topup_pipeline(self, **name_maps):
+
+        pipeline = self.new_pipeline(
+            name='preprocess_pipeline',
+            desc=("Topup distortion correction pipeline"),
+            citations=[fsl_cite],
+            name_maps=name_maps)
+
+        reorient_epi_in = pipeline.add(
+            'reorient_epi_in',
+            fsl.utils.Reorient2Std(),
+            inputs={
+                'in_file': ('series', nifti_gz_format)},
+            requirements=[fsl_req.v('5.0.9')])
+
+        reorient_epi_opposite = pipeline.add(
+            'reorient_epi_opposite',
+            fsl.utils.Reorient2Std(),
+            inputs={
+                'in_file': ('reverse_phase', nifti_gz_format)},
+            requirements=[fsl_req.v('5.0.9')])
+
+        prep_dwi = pipeline.add(
+            'prepare_dwi',
+            PrepareDWI(
+                topup=True),
+            inputs={
+                'pe_dir': ('ped', str),
+                'ped_polarity': ('pe_angle', str),
+                'dwi': (reorient_epi_in, 'out_file'),
+                'dwi1': (reorient_epi_opposite, 'out_file')})
+
+        ped = pipeline.add(
+            'gen_config',
+            GenTopupConfigFiles(),
+            inputs={
+                'ped': (prep_dwi, 'pe')})
+
+        merge_outputs = pipeline.add(
+            'merge_files',
+            merge_lists(2),
+            inputs={
+                'in1': (prep_dwi, 'main'),
+                'in2': (prep_dwi, 'secondary')})
+
+        merge = pipeline.add(
+            'fsl_merge',
+            fsl_merge(
+                dimension='t',
+                output_type='NIFTI_GZ'),
+            inputs={
+                'in_files': (merge_outputs, 'out')},
+            requirements=[fsl_req.v('5.0.9')])
+
+        topup = pipeline.add(
+            'topup',
+            TOPUP(
+                output_type='NIFTI_GZ'),
+            inputs={
+                'in_file': (merge, 'merged_file'),
+                'encoding_file': (ped, 'config_file')},
+            requirements=[fsl_req.v('5.0.9')])
+
+        in_apply_tp = pipeline.add(
+            'in_apply_tp',
+            merge_lists(1),
+            inputs={
+                'in1': (reorient_epi_in, 'out_file')})
+
+        pipeline.add(
+            'applytopup',
+            ApplyTOPUP(
+                method='jac',
+                in_index=[1],
+                output_type='NIFTI_GZ'),
+            inputs={
+                'in_files': (in_apply_tp, 'out'),
+                'encoding_file': (ped, 'apply_topup_config'),
+                'in_topup_movpar': (topup, 'out_movpar'),
+                'in_topup_fieldcoef': (topup, 'out_fieldcoef')},
+            outputs={
+                'series_preproc': ('out_corrected', nifti_gz_format)},
+            requirements=[fsl_req.v('5.0.9')])
+
+        return pipeline
+
+    def _fugue_pipeline(self, **name_maps):
+
+        pipeline = self.new_pipeline(
+            name='preprocess_pipeline',
+            desc=("Fugue distortion correction pipeline"),
+            citations=[fsl_cite],
+            name_maps=name_maps)
+
+        reorient_epi_in = pipeline.add(
+            'reorient_epi_in',
+            fsl.utils.Reorient2Std(
+                output_type='NIFTI_GZ'),
+            inputs={
+                'in_file': ('series', nifti_gz_format)},
+            requirements=[fsl_req.v('5.0.9')])
+
+        fm_mag_reorient = pipeline.add(
+            'reorient_fm_mag',
+            fsl.utils.Reorient2Std(
+                output_type='NIFTI_GZ'),
+            inputs={
+                'in_file': ('field_map_mag', nifti_gz_format)},
+            requirements=[fsl_req.v('5.0.9')])
+
+        fm_phase_reorient = pipeline.add(
+            'reorient_fm_phase',
+            fsl.utils.Reorient2Std(
+                output_type='NIFTI_GZ'),
+            inputs={
+                'in_file': ('field_map_phase', nifti_gz_format)},
+            requirements=[fsl_req.v('5.0.9')])
+
+        bet = pipeline.add(
+            "bet",
+            BET(
+                robust=True,
+                output_type='NIFTI_GZ'),
+            inputs={
+                'in_file': (fm_mag_reorient, 'out_file')},
+            wall_time=5,
+            requirements=[fsl_req.v('5.0.9')])
+
+        create_fmap = pipeline.add(
+            "prepfmap",
+            PrepareFieldmap(
+                # delta_TE=2.46
+            ),
+            inputs={
+                'delta_TE': ('field_map_delta_te', float),
+                "in_magnitude": (bet, "out_file"),
+                'in_phase': (fm_phase_reorient, 'out_file')},
+            wall_time=5,
+            requirements=[fsl_req.v('5.0.9')])
+
+        pipeline.add(
+            'fugue',
+            FUGUE(
+                unwarp_direction='x',
+                dwell_time=self.parameter('fugue_echo_spacing'),
+                unwarped_file='example_func.nii.gz',
+                output_type='NIFTI_GZ'),
+            inputs={
+                'fmap_in_file': (create_fmap, 'out_fieldmap'),
+                'in_file': (reorient_epi_in, 'out_file')},
+            outputs={
+                'series_preproc': ('unwarped_file', nifti_gz_format)},
+            wall_time=5,
+            requirements=[fsl_req.v('5.0.9')])
+
+        return pipeline
+
+    def field_map_time_info_pipeline(self, **name_maps):
+
+        pipeline = self.new_pipeline(
+            name='field_map_time_info_pipeline',
+            desc=("Pipeline to extract delta TE from field map "
+                  "images, if provided"),
+            citations=[fsl_cite],
+            name_maps=name_maps)
+
+        pipeline.add(
+            'extract_delta_te',
+            FieldMapTimeInfo(),
+            inputs={
+                'fm_mag': ('field_map_mag', dicom_format)},
+            outputs={
+                'field_map_delta_te': ('delta_te', float)})
+
+        return pipeline
+
 
 class EpiSeriesStudy(EpiStudy, metaclass=StudyMetaClass):
 
@@ -251,192 +437,6 @@ class EpiSeriesStudy(EpiStudy, metaclass=StudyMetaClass):
                 'file_list': (mcflirt, 'mat_file')},
             outputs={
                 'align_mats': ('out_dir', motion_mats_format)})
-
-        return pipeline
-
-    def field_map_time_info_pipeline(self, **name_maps):
-
-        pipeline = self.new_pipeline(
-            name='field_map_time_info_pipeline',
-            desc=("Pipeline to extract delta TE from field map "
-                  "images, if provided"),
-            citations=[fsl_cite],
-            name_maps=name_maps)
-
-        pipeline.add(
-            'extract_delta_te',
-            FieldMapTimeInfo(),
-            inputs={
-                'fm_mag': ('field_map_mag', dicom_format)},
-            outputs={
-                'field_map_delta_te': ('delta_te', float)})
-
-        return pipeline
-
-    def preprocess_pipeline(self, **name_maps):
-
-        if ('field_map_phase' in self.input_names and
-                'field_map_mag' in self.input_names):
-            return self._fugue_pipeline(**name_maps)
-        elif 'reverse_phase' in self.input_names:
-            return self._topup_pipeline(**name_maps)
-        else:
-            return super().preprocess_pipeline(**name_maps)
-
-    def _topup_pipeline(self, **name_maps):
-
-        pipeline = self.new_pipeline(
-            name='preprocess_pipeline',
-            desc=("Topup distortion correction pipeline"),
-            citations=[fsl_cite],
-            name_maps=name_maps)
-
-        reorient_epi_in = pipeline.add(
-            'reorient_epi_in',
-            fsl.utils.Reorient2Std(),
-            inputs={
-                'in_file': ('series', nifti_gz_format)},
-            requirements=[fsl_req.v('5.0.9')])
-
-        reorient_epi_opposite = pipeline.add(
-            'reorient_epi_opposite',
-            fsl.utils.Reorient2Std(),
-            inputs={
-                'in_file': ('reverse_phase', nifti_gz_format)},
-            requirements=[fsl_req.v('5.0.9')])
-
-        prep_dwi = pipeline.add(
-            'prepare_dwi',
-            PrepareDWI(
-                topup=True),
-            inputs={
-                'pe_dir': ('ped', str),
-                'ped_polarity': ('pe_angle', str),
-                'dwi': (reorient_epi_in, 'out_file'),
-                'dwi1': (reorient_epi_opposite, 'out_file')})
-
-        ped = pipeline.add(
-            'gen_config',
-            GenTopupConfigFiles(),
-            inputs={
-                'ped': (prep_dwi, 'pe')})
-
-        merge_outputs = pipeline.add(
-            'merge_files',
-            merge_lists(2),
-            inputs={
-                'in1': (prep_dwi, 'main'),
-                'in2': (prep_dwi, 'secondary')})
-
-        merge = pipeline.add(
-            'fsl_merge',
-            fsl_merge(
-                dimension='t',
-                output_type='NIFTI_GZ'),
-            inputs={
-                'in_files': (merge_outputs, 'out')},
-            requirements=[fsl_req.v('5.0.9')])
-
-        topup = pipeline.add(
-            'topup',
-            TOPUP(
-                output_type='NIFTI_GZ'),
-            inputs={
-                'in_file': (merge, 'merged_file'),
-                'encoding_file': (ped, 'config_file')},
-            requirements=[fsl_req.v('5.0.9')])
-
-        in_apply_tp = pipeline.add(
-            'in_apply_tp',
-            merge_lists(1),
-            inputs={
-                'in1': (reorient_epi_in, 'out_file')})
-
-        pipeline.add(
-            'applytopup',
-            ApplyTOPUP(
-                method='jac',
-                in_index=[1],
-                output_type='NIFTI_GZ'),
-            inputs={
-                'in_files': (in_apply_tp, 'out'),
-                'encoding_file': (ped, 'apply_topup_config'),
-                'in_topup_movpar': (topup, 'out_movpar'),
-                'in_topup_fieldcoef': (topup, 'out_fieldcoef')},
-            outputs={
-                'series_preproc': ('out_corrected', nifti_gz_format)},
-            requirements=[fsl_req.v('5.0.9')])
-
-        return pipeline
-
-    def _fugue_pipeline(self, **name_maps):
-
-        pipeline = self.new_pipeline(
-            name='preprocess_pipeline',
-            desc=("Fugue distortion correction pipeline"),
-            citations=[fsl_cite],
-            name_maps=name_maps)
-
-        reorient_epi_in = pipeline.add(
-            'reorient_epi_in',
-            fsl.utils.Reorient2Std(
-                output_type='NIFTI_GZ'),
-            inputs={
-                'in_file': ('series', nifti_gz_format)},
-            requirements=[fsl_req.v('5.0.9')])
-
-        fm_mag_reorient = pipeline.add(
-            'reorient_fm_mag',
-            fsl.utils.Reorient2Std(
-                output_type='NIFTI_GZ'),
-            inputs={
-                'in_file': ('field_map_mag', nifti_gz_format)},
-            requirements=[fsl_req.v('5.0.9')])
-
-        fm_phase_reorient = pipeline.add(
-            'reorient_fm_phase',
-            fsl.utils.Reorient2Std(
-                output_type='NIFTI_GZ'),
-            inputs={
-                'in_file': ('field_map_phase', nifti_gz_format)},
-            requirements=[fsl_req.v('5.0.9')])
-
-        bet = pipeline.add(
-            "bet",
-            BET(
-                robust=True,
-                output_type='NIFTI_GZ'),
-            inputs={
-                'in_file': (fm_mag_reorient, 'out_file')},
-            wall_time=5,
-            requirements=[fsl_req.v('5.0.9')])
-
-        create_fmap = pipeline.add(
-            "prepfmap",
-            PrepareFieldmap(
-                # delta_TE=2.46
-            ),
-            inputs={
-                'delta_TE': ('field_map_delta_te', float),
-                "in_magnitude": (bet, "out_file"),
-                'in_phase': (fm_phase_reorient, 'out_file')},
-            wall_time=5,
-            requirements=[fsl_req.v('5.0.9')])
-
-        pipeline.add(
-            'fugue',
-            FUGUE(
-                unwarp_direction='x',
-                dwell_time=self.parameter('fugue_echo_spacing'),
-                unwarped_file='example_func.nii.gz',
-                output_type='NIFTI_GZ'),
-            inputs={
-                'fmap_in_file': (create_fmap, 'out_fieldmap'),
-                'in_file': (reorient_epi_in, 'out_file')},
-            outputs={
-                'series_preproc': ('unwarped_file', nifti_gz_format)},
-            wall_time=5,
-            requirements=[fsl_req.v('5.0.9')])
 
         return pipeline
 
