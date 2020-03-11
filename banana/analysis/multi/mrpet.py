26cabf820791cb67f5b486e24101a83d2bdb5972
Tom Close <tom.g.close@gmail.com>
2020-03-11 16:04:05 +1100

    renamed SubAnalysisSpec to SubCompSpec

--- banana/analysis/multi/mrpet.py
+++ banana/analysis/multi/mrpet.py
@@ -1,1 +1,1 @@
-/annex/objects/SHA256E-s46906--8c340805af4dda89808b4e64403ad0eeea5d884e5f1cea561c33c40a89aa340a.py
+/annex/objects/SHA256E-s46822--ffc4d7ebd72caf4fdbc63bb741c079fdb51017be71b6241b77fbf467f42f50d8.py
